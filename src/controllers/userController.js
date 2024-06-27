ca78b5a22a4a55753c2da39e493f036172c6ddf4
Abel3581 <marzoa3581@gmail.com>
2024-06-27 00:36:22 -0300

    Agregado userRoutes y terminado model user y registro de usuarios

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,7 @@ import User from '../models/User.js';
 export const createUser = async (req, res) => {
     const request = req.body;
     try {
-        const response = await User.Save();
+        const response = await User.create(request);
         return res.status(201).json(response);
     } catch (error) {
         res.status(400).json({error: error.message});
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,2 +1,8 @@
 import { Router } from "express";
+import  * as userController from '../controllers/userController.js';
 
+const router = Router();
+
+router.post('/register', userController.createUser)
+
+export default router;
