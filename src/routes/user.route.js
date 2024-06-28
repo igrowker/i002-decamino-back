ee2916ba0228a0abae88e9153c0bf2d14014b071
Renzo Demarco <renzo11demarco@gmail.com>
2024-06-27 21:01:05 -0300

    Revert "Merge pull request #20 from igrowker/auth"
    This reverts commit cd32b276489b8251d77bb91768c3d53c9d9c9713, reversing
    changes made to 5d337dd812be8474671f1a2ce1f654dd1a1be562.

--- src/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import  * as userController from '../controllers/user.controller.js';
-
-const router = Router();
-
-router.post('/register', userController.POSTUserRegister)
-.post('/login', userController.POSTUserLogin)
-
-export default router;
--- src/utils/jwt.js
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken'
-import env from '../config/env.js'
-
-const SECRET = env.JWT_SECRET
-
-export const generateToken = object => jwt.sign(object, SECRET, { expiresIn: '1h' })
-
