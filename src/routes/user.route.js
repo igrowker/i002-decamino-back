ac3e5d9e8b7c70b1dcc8627b84ddb0d9994d4cb9
Abel3581 <marzoa3581@gmail.com>
2024-06-28 16:56:36 -0300

    Agregado config swagger y mapeado el registro de usuario con el mismo

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,54 @@
 import { Router } from "express";
-import  * as userController from '../controllers/user.controller.js';
+import * as userController from '../controllers/user.controller.js';
+import User from "../models/User.js";
 
 const router = Router();
-
+/**
+ * @swagger
+ * /user/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/UserInput'
+ *     responses:
+ *       201:
+ *         description: Usuario creado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message
+ *               example:
+ *                 error: El nombre de usuario es requerido
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: 500Internal Server Error
+ *               example:
+ *                 error: "E11000 duplicate key error collection: test.users index: username_1 dup key: { username: \"sasassalkasqwqaslk\" }"
+ */
 router.post('/register', userController.POSTUserRegister)
-.post('/login', userController.POSTUserLogin)
+
+router.post('/login', userController.POSTUserLogin)
 
 export default router;
