5e1bd58e6ef4fdb3ab4db7613fb03da3a020d276
Renzo Demarco <renzo11demarco@gmail.com>
2024-07-22 10:49:21 -0300

    agregado timestamps en payments

--- src/models/payment.model.js
+++ src/models/payment.model.js
@@ -18,11 +18,9 @@ const paymentSchema = new mongoose.Schema({
   amount: {
     type: Number,
     required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
   }
+}, {
+  timestamps: true
 });
 
 const Payment = mongoose.model('Payment', paymentSchema);
