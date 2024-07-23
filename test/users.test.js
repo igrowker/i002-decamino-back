62f038007bd5e39f252aabc0b0c43b947464f2a6
Renzo Demarco <renzo11demarco@gmail.com>
2024-07-23 13:17:46 -0300

    agregado testing

--- test/users.test.js
+++ test/users.test.js
@@ -1,9 +1,20 @@
 import { expect } from 'chai'
 import supertest from 'supertest'
+import '../src/config/env.js'
 
-const requester = supertest(process.env.BASE_URL + '/api/user')
+const requester = supertest(process.env.BASE_URL + "/api")
 
 describe("Testeando un flujo de operaciones para Users...", () => {
-  const email = "renzo@gmail.com"
-  let cookie
+
+  const email = "testing@gmail.com"
+  const password = "12345678"
+
+  it("Debería registrar un usuario", async () => {
+    const data = { username: "Test de User", email, password, role: "merchant" }
+    const response = await requester.post('/user/register').send(data)
+    const { statusCode, body } = response
+    expect(statusCode).to.be.equals(201)
+    expect(body).to.have.property('_id');
+})
+
 })
