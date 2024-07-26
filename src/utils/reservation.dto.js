5dbbfca2200787072176f65796ece804d95e5a23
Renzo Demarco <renzo11demarco@gmail.com>
2024-07-26 16:34:13 -0300

    agregado restaurantId en ReservationDTO

--- src/utils/reservation.dto.js
+++ src/utils/reservation.dto.js
@@ -3,6 +3,7 @@ export default class ReservationDTO {
     this.id = reservation._id
     this.user = reservation.user
     this.restaurant = reservation.restaurant.title
+    this.restaurantId =  reservation.restaurant._id
     this.date = reservation.date
     this.numberOfPeople = reservation.numberOfPeople
     this.status = reservation.status
